2a88f8016caff2cede2d95653499982de981e701
shaikh soheel <shaikhsoheel185@gmail.com>
2022-12-18 18:47:55 +0530

    commit

---
+++ LinkedList/src/LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+//Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+
+
+public class MiddleOfLinkedList {
+
+                   /*   public ListNode middleNode(ListNode head)
+
+                   {
+                        ListNode slow = head, fast = head;
+                        ListNode prev = slow;
+
+                         while (fast!=null && fast.next != null)
+                         {
+
+                         prev = slow;
+                         fast = fast.next.next;
+                         slow = slow.next;
+
+                     }
+
+                  return slow;
+             }*/
+
+
+
+    // TC: O(n) where n is the nodes in the linked list.
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ LinkedList/src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,55 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+
+   /*     public static Node reverse linked-list (head) {
+
+   *           Node CurrentNode = Head;                          1 - 2 - 3 - 4 - 5 - null;
+   *           Node prev        = null;                          5 - 4 - 3 - 2 - 1 - null;
+   *           Node next        = null;
+   *
+               while( CurrentNode != null ) {
+
+               next = CurrentNode.next;
+               current.next = prev;
+
+               prev = currentNode;
+               currentNode = next;
+
+               }
+
+               Node = prev;
+
+               return Node ;
+   *
+   *     }
+   *
+   *       Tc = O  ( n );               n is number of Nodes in linked list
+   *       Sc = O  ( 1 );               space complexity is constant
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   * */
+}
